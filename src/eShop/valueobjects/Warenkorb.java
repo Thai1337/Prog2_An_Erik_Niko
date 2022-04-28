bfaf23285784e4d397655e90d04af714b1a6583c
EJHunter07-07 <73643704+EJHunter07-07@users.noreply.github.com>
2022-04-28 14:30:30 +0200

    Getter Setter
    Getter Setter zu Gesamtpreis

--- src/eShop/valueobjects/Warenkorb.java
+++ src/eShop/valueobjects/Warenkorb.java
@@ -3,4 +3,11 @@ package eShop.valueobjects;
 public class Warenkorb {
     private double gesamtpreis;
 
+    public double getGesamtpreis() {
+        return gesamtpreis;
+    }
+
+    public void setGesamtpreis(double gesamtpreis) {
+        this.gesamtpreis = gesamtpreis;
+    }
 }
